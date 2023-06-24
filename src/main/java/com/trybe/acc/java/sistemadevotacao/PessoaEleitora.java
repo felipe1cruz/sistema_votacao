da69b6383c273aa6813917d941dd45a5a0a2f66a
Felipe Moreira da Cruz <piradous@gmail.com>
2023-06-24 16:02:14 -0300

    feat: Classe pessoa Eleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,32 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+/**
+ * Classe pessoa Eleitora.
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+
 
 }
