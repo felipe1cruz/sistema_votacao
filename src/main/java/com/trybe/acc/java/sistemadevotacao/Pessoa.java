418fc737dbba34418f888da79ca975fc9734acf8
Felipe Moreira da Cruz <piradous@gmail.com>
2023-06-24 16:01:12 -0300

    feat: classe abstrata Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -1,5 +1,17 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class Pessoa {
+/**
+ * Classe pessoa.
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  public abstract String getNome();
+
+  public abstract void setNome(String nome);
 
 }
